0ef1dcafa8a96eb801e57544895b31ec0e7e2f77
Sander <sanderdsz@gmail.com>
2022-04-07 19:59:39 -0300

    header component done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import {Flex, Icon, Input, Text} from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import {Flex, Icon, Input, HStack, Text, Box, Avatar} from '@chakra-ui/react'
+import {RiNotificationLine, RiSearchLine, RiUserAddLine} from 'react-icons/ri'
 
 export function Header () {
   return (
@@ -41,7 +41,56 @@ export function Header () {
             placeholder='Search on platform'
             _placeholder={{color: 'gray.50'}}
           />
-          <Icon as={RiSearchLine} fontSize='20' />
+          <Icon
+            as={RiSearchLine}
+            fontSize='20'
+          />
+        </Flex>
+        <Flex
+          align='center'
+          ml='auto'
+        >
+          <HStack
+            spacing='4'
+            mx='8'
+            pr='8'
+            py='1'
+            color='gray.300'
+            borderRightWidth={1}
+            borderColor='gray.700'
+          >
+            <Icon
+              as={RiNotificationLine}
+              fontSize='20'
+            />
+            <Icon
+              as={RiUserAddLine}
+              fontSize='20'
+            />
+          </HStack>
+          <Flex
+            align='center'
+          >
+            <Box>
+              <Text
+                mr='4'
+                textAlign='right'
+              >
+                Sander
+              </Text>
+              <Text
+                color='gray.300'
+                fontSize='small'
+              >
+                sanderdsz@gmail.com
+              </Text>
+            </Box>
+            <Avatar
+              size='md'
+              name='Sander Zuchinalli'
+              src='https://github.com/sanderdsz.png'
+            />
+          </Flex>
         </Flex>
       </Flex>
     </>
