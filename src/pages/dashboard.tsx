3f7ff46a1f6b7338eee4022d96c7ec001affcdd5
sanderdsz <sanderdsz@gmail.com>
2022-04-08 11:12:23 -0300

    dashboard and apexcharts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,66 @@
 import { Header } from '../components/Header'
-import {Box, Flex, SimpleGrid, Text} from "@chakra-ui/react";
+import {Box, Flex, SimpleGrid, Text, theme} from "@chakra-ui/react";
 import {Sidebar} from "../components/Sidebar";
+import dynamic from "next/dynamic";
+// Lazyload
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+})
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  tooltip: {
+    enable: false
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021-03-18T00:00:00.000Z',
+      '2021-03-19T00:00:00.000Z',
+      '2021-03-20T00:00:00.000Z',
+      '2021-03-21T00:00:00.000Z',
+      '2021-03-22T00:00:00.000Z',
+      '2021-03-23T00:00:00.000Z',
+      '2021-03-24T00:00:00.000Z'
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3
+    }
+  }
+}
+
+const series = [
+  {
+    name: 'series1',
+    data: [31, 120, 124, 10, 28, 15, 88]
+  }
+]
 
 export default function Dashboard () {
  return (
@@ -27,7 +87,7 @@ export default function Dashboard () {
            p='8'
            bgColor='gray.800'
            borderRadius={8}
-           //pb='4'
+           pb='4'
          >
             <Text
               fontSize='lg'
@@ -35,6 +95,12 @@ export default function Dashboard () {
             >
               Weekly subscribers
             </Text>
+           <Chart
+            type='area'
+            height={160}
+            options={options}
+            series={series}
+           />
          </Box>
          <Box
            p='8'
@@ -48,6 +114,12 @@ export default function Dashboard () {
            >
              Opening taxes
            </Text>
+           <Chart
+             type='area'
+             height={160}
+             options={options}
+             series={series}
+           />
          </Box>
        </SimpleGrid>
      </Flex>
