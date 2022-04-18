532b3d712d1c2dcae3cda1dc4a65a716a3505562
sanderdsz <sanderdsz@gmail.com>
2022-04-18 20:24:34 -0300

    miragejs and faker data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,24 @@
-import { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '../styles/theme'
+import {AppProps} from 'next/app'
+import {ChakraProvider} from '@chakra-ui/react'
+import {theme} from '../styles/theme'
 import {SidebarDrawerProvider} from "../contexts/SidebarDrawerContext";
+import {makeServer} from "../services/mirage";
+import {QueryClient, QueryClientProvider} from 'react-query'
 
-function MyApp({ Component, pageProps }: AppProps) {
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
+
+const queryClient = new QueryClient()
+
+function MyApp({Component, pageProps}: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
+      <QueryClientProvider client={queryClient}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,7 +4,7 @@ import {
   Checkbox,
   Flex,
   Heading,
-  Icon,
+  Icon, Spinner,
   Table,
   Tbody,
   Td,
@@ -19,13 +19,19 @@ import {Sidebar} from "../../components/Sidebar";
 import {RiAddLine, RiPencilLine} from "react-icons/ri";
 import {Pagination} from "../../components/Pagination";
 import Link from 'next/link'
+import {useEffect} from "react";
+import {useQuery} from 'react-query'
 
 export default function UserList() {
+  const {data, isLoading, error} = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = response.json()
+    return data
+  })
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
-
   return (
     <>
       <Box>
@@ -67,60 +73,74 @@ export default function UserList() {
                 </Button>
               </Link>
             </Flex>
-            <Table
-              colorScheme='whiteAlpha'
-            >
-              <Thead>
-                <Tr>
-                  <Th
-                    px={['4', '4', '6']}
-                    color='gray.300'
-                    width='8'
-                  >
-                    <Checkbox colorScheme='pink'/>
-                  </Th>
-                  <Th>User</Th>
-                  {isWideVersion && <Th>Registration date</Th>}
-                  {isWideVersion && <Th width='8'></Th>}
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td px={['4', '4', '6']}>
-                    <Checkbox colorScheme='pink'/>
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight='bold'>Sander Zuchinalli</Text>
-                      <Text
-                        fontSize='sm'
+            {isLoading ? (
+              <Flex justify='center'>
+                <Spinner/>
+              </Flex>
+            ) : error ? (
+              <Flex justify='center'>
+                <Text>
+                  Failed to fetch users data
+                </Text>
+              </Flex>
+            ) : (
+              <>
+                <Table
+                  colorScheme='whiteAlpha'
+                >
+                  <Thead>
+                    <Tr>
+                      <Th
+                        px={['4', '4', '6']}
                         color='gray.300'
+                        width='8'
                       >
-                        sanderdsz@gmail.com
-                      </Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion &&
-                      <Td>
-                          April 04, 2021
+                        <Checkbox colorScheme='pink'/>
+                      </Th>
+                      <Th>User</Th>
+                      {isWideVersion && <Th>Registration date</Th>}
+                      {isWideVersion && <Th width='8'></Th>}
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink'/>
                       </Td>
-                  }
-                  {isWideVersion &&
                       <Td>
-                          <Button
-                              as='a'
-                              size='sm'
-                              fontSize='sm'
-                              colorScheme='purple'
-                              leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                        <Box>
+                          <Text fontWeight='bold'>Sander Zuchinalli</Text>
+                          <Text
+                            fontSize='sm'
+                            color='gray.300'
                           >
-                              Edit
-                          </Button>
+                            sanderdsz@gmail.com
+                          </Text>
+                        </Box>
                       </Td>
-                  }
-                </Tr>
-              </Tbody>
-            </Table>
+                      {isWideVersion &&
+                          <Td>
+                              April 04, 2021
+                          </Td>
+                      }
+                      {isWideVersion &&
+                          <Td>
+                              <Button
+                                  as='a'
+                                  size='sm'
+                                  fontSize='sm'
+                                  colorScheme='purple'
+                                  leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                              >
+                                  Edit
+                              </Button>
+                          </Td>
+                      }
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </>
+            )}
             <Pagination/>
           </Box>
         </Flex>
